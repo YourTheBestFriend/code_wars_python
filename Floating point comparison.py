6c9a10e94c99780fee3bbc9258e1e8c9f7127a2e
Xhref <goncharovgithub8@gmail.com>
2021-08-23 19:52:05 +0300

    task floating point task 70% ready

--- Floating point comparison.py
+++ Floating point comparison.py
@@ -1,1 +1,9 @@
-print('test')
+# Floating point comparison.py
+def approx_equals(a, b):
+    a = round(a, 4)
+    b = round(b, 4)
+    print(a, ' ', b)
+    if (a == b or (a + 0.001 == b or a - 0.001 == b)) and int(a) == int(b):
+        return True
+    else:       
+        return False
